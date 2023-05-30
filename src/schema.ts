f7dc28bc8f8d7483a00c7d8aa74468f396e362b9
hr-h.sugiyama <s7.harubright@hoshinoresorts.com>
2023-05-30 16:25:01 +0900

    fix: 型を設定し、index.tsを作成、名前を変更した

---
+++ src/schema.ts
@@ -0,0 +1,52 @@
+export interface IdTitle {
+  id: string
+  title: string
+}
+
+export interface Element extends IdTitle {
+  start: Date
+  end: Date
+  style: Record<string, string>
+}
+
+export interface TrackElement extends IdTitle {
+  elements: Element[]
+}
+
+export interface Track extends IdTitle {
+  elements: Element[]
+  tracks: TrackElement[]
+  hasButton?: boolean
+  link?: string
+  isOpen?: boolean
+}
+
+export type ToggleTrackOpen = (track: Track) => void
+export type ClickTrackButton = (track: Track) => JSX.Element
+export type CornerDomAdd = () => JSX.Element
+
+export interface Scale {
+  start: Date
+  end: Date
+  zoom: number
+  zoomMin?: number
+  zoomMax?: number
+  minWidth?: number
+}
+
+export interface TimeCell extends IdTitle {
+  start: Date
+  end: Date
+}
+
+export interface Sticky {
+  isSticky: boolean
+  headerHeight: number
+  sidebarWidth: number
+}
+
+export interface Timebar extends IdTitle {
+  cells: TimeCell[]
+  useAsGrid?: boolean
+  style: Record<string, string>
+}
